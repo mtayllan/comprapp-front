24e6f5058373db059dc9d07b155a38d7f447b4bf
mtayllan <messiastayllan@gmail.com>
2020-01-08 15:10:11 -0300

    Adjust login page to be more simple

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,29 +1,154 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { loader } from 'graphql.macro';
-import { authClient } from '../../services/apollo';
+import React, { useState, useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import validate from 'validate.js';
+import {
+  Grid,
+  Button,
+  TextField,
+  Typography,
+} from '@material-ui/core';
+import { useStyles } from './styles-jss';
 
-const AdminLoginMutation = loader('./operations.gql');
+const schema = {
+  email: {
+    presence: { allowEmpty: false, message: 'is required' },
+    email: true,
+    length: {
+      maximum: 64,
+    },
+  },
+  password: {
+    presence: { allowEmpty: false, message: 'is required' },
+    length: {
+      maximum: 128,
+    },
+  },
+};
 
-export default function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [adminLogin, { data }] = useMutation(AdminLoginMutation, { client: authClient });
+const SignIn = (props) => {
+  const { history } = props;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    adminLogin({ variables: { email, password } });
+  const classes = useStyles();
+
+  const [formState, setFormState] = useState({
+    isValid: false,
+    values: {},
+    touched: {},
+    errors: {},
+  });
+
+  useEffect(() => {
+    const errors = validate(formState.values, schema);
+
+    setFormState((prevFormState) => ({
+      ...prevFormState,
+      isValid: !errors,
+      errors: errors || {},
+    }));
+  }, [formState.values]);
+
+  const handleChange = (event) => {
+    event.persist();
+
+    setFormState((prevFormState) => ({
+      ...prevFormState,
+      values: {
+        ...prevFormState.values,
+        [event.target.name]:
+          event.target.type === 'checkbox'
+            ? event.target.checked
+            : event.target.value,
+      },
+      touched: {
+        ...prevFormState.touched,
+        [event.target.name]: true,
+      },
+    }));
   };
 
+  const handleSignIn = (event) => {
+    event.preventDefault();
+    history.push('/');
+  };
+
+  const hasError = (field) => (!!(formState.touched[field] && formState.errors[field]));
+
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <input type="email" onChange={(e) => setEmail(e.target.value)} />
-          <input type="password" onChange={(e) => setPassword(e.target.value)} />
-        </div>
-        <input type="submit" value="Entrar" />
-      </form>
+    <div className={classes.root}>
+      <Grid
+        className={classes.grid}
+        justify="center"
+        container
+      >
+        <Grid
+          className={classes.content}
+          item
+          lg={7}
+          xs={12}
+        >
+          <div className={classes.content}>
+            <div className={classes.contentBody}>
+              <form
+                className={classes.form}
+                onSubmit={handleSignIn}
+              >
+                <Typography
+                  className={classes.title}
+                  variant="h2"
+                >
+                  Acessar o sistema
+                </Typography>
+                <TextField
+                  className={classes.textField}
+                  error={hasError('email')}
+                  fullWidth
+                  helperText={
+                    hasError('email') ? formState.errors.email[0] : null
+                  }
+                  label="Email"
+                  name="email"
+                  onChange={handleChange}
+                  type="text"
+                  value={formState.values.email || ''}
+                  variant="outlined"
+                />
+                <TextField
+                  className={classes.textField}
+                  error={hasError('password')}
+                  fullWidth
+                  helperText={
+                    hasError('password') ? formState.errors.password[0] : null
+                  }
+                  label="Senha"
+                  name="password"
+                  onChange={handleChange}
+                  type="password"
+                  value={formState.values.password || ''}
+                  variant="outlined"
+                />
+                <Button
+                  className={classes.signInButton}
+                  color="primary"
+                  disabled={!formState.isValid}
+                  fullWidth
+                  size="large"
+                  type="submit"
+                  variant="contained"
+                >
+                  Entrar
+                </Button>
+              </form>
+            </div>
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
-}
+};
+
+SignIn.propTypes = {
+  history: PropTypes.object.isRequired,
+};
+
+export default withRouter(SignIn);
